4dae457b3a60e16bd3556e06525b7af0b67d8018
David Shull <davidqshull@gmail.com>
2016-10-06 04:26:29 -0400

    add right hand side table functionality

--- parser_generator.cpp
+++ parser_generator.cpp
@@ -10,7 +10,11 @@ int number_of_terminals;
 int number_of_nonterminals;
 int number_of_symbols;
 vector<string> terminals;
+vector<int> terminal_numbers;
 vector<string> nonterminals;
+vector<int> nonterminal_numbers;
+vector< vector<int> > right_hand_sides;
+
 
 vector<string> split(const char *str, char c) {
     vector<string> result;
@@ -35,24 +39,80 @@ vector<string> read_file(string filename) {
     return contents;
 }
 
+void fill_terminals(vector<string> contents) {
+    for(int i = 0; i < number_of_terminals; i++) {
+        vector<string> temp = split(contents[i].c_str(), ' ');
+
+        if(find(terminals.begin(), terminals.end(), temp[0]) == terminals.end()) {
+            terminals.push_back(temp[0]);
+            for(int i = 1; i < temp.size(); i++) {
+                if(!temp[i].empty())
+                    terminal_numbers.push_back(atoi(temp[i].c_str()));
+            }
+        }
+    }
+}
+
+void fill_nonterminals(vector<string> productions) {
+    int count = 0;
+    for(int i = 0; i < number_of_productions; i++) {
+        vector<string> temp = split(productions[i].c_str(), ' ');
+        if(find(nonterminals.begin(), nonterminals.end(), temp[0]) == nonterminals.end()) {
+            count++;
+            nonterminals.push_back(temp[0]);
+            nonterminal_numbers.push_back(count);
+        }
+    }
+}
+
+void set_right_hand_sides(vector<string> productions, vector<string> contents) {
+    vector<int> first;
+    first.push_back(0);
+    right_hand_sides.push_back(first);
+
+    for(int i = 0; i < number_of_productions; i++) {
+        vector<string> temp = split(productions[i].c_str(), ' ');
+        vector<string> rhs_strings;
+
+        if(temp.size() < 3) {
+            rhs_strings.push_back("0");
+        }
+
+        for(int j = 2; j < temp.size(); j++)
+            rhs_strings.push_back(temp[j]);
+
+        vector<int> rhs;
+        rhs.push_back(0);
+        for(int i = 0; i < rhs_strings.size(); i++) {
+            if(find(terminals.begin(), terminals.end(), rhs_strings[i]) != terminals.end()) {
+                int pos = find(terminals.begin(), terminals.end(), rhs_strings[i]) - terminals.begin();
+                rhs.push_back(0 - terminal_numbers[pos]);
+            }
+            else if(find(nonterminals.begin(), nonterminals.end(), rhs_strings[i]) != nonterminals.end()) {
+                int pos = find(nonterminals.begin(), nonterminals.end(), rhs_strings[i]) - nonterminals.begin();
+                rhs.push_back(nonterminal_numbers[pos]);
+            }
+        }
+        for(int i: rhs) {
+            cout << i;
+            cout << " ";
+        }
+        cout << endl;
+        right_hand_sides.push_back(rhs);
+    }
+}
+
 void set_variables(vector<string> contents, vector<string> productions) {
     number_of_terminals = contents.size() - 1;
     number_of_productions = productions.size();
 
-    for(int i = 0; i < number_of_terminals; i++) {
-        vector<string> temp = split(contents[i].c_str(), ' ');
-        if(std::find(terminals.begin(), terminals.end(), temp[0]) == terminals.end())
-            terminals.push_back(temp[0]);
-    }
-
-    for(int i = 0; i < number_of_productions; i++) {
-        vector<string> temp = split(productions[i].c_str(), ' ');
-        if(std::find(nonterminals.begin(), nonterminals.end(), temp[0]) == nonterminals.end())
-            nonterminals.push_back(temp[0]);
-    }
+    fill_terminals(contents);
+    fill_nonterminals(productions);
 
     number_of_nonterminals = nonterminals.size();
     number_of_symbols = number_of_nonterminals + number_of_terminals;
+
+    set_right_hand_sides(productions, contents);
 }
 
 int main() {
