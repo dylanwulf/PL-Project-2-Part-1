e65515979b26073a6af4338f4e08e85ad9907b89
David Shull <davidqshull@gmail.com>
2016-10-06 01:36:39 -0400

    Add Makefile, add set_variables function

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	g++ -o parser_generator parser_generator.cpp
+
+clean:
+	rm parser_generator
--- parser_generator.cpp
+++ parser_generator.cpp
@@ -5,6 +5,22 @@
 #include <algorithm>
 using namespace std;
 
+int number_of_productions;
+int number_of_tokens;
+vector<string> terminals;
+vector<string> nonterminals;
+
+vector<string> split(const char *str, char c) {
+    vector<string> result;
+    do {
+        const char *begin = str;
+        while(*str != c && *str)
+            str++;
+        result.push_back(string(begin, str));
+    } while (0 != *str++);
+    return result;
+}
+
 vector<string> read_file(string filename) {
     vector<string> contents;
     string line;
@@ -17,16 +33,35 @@ vector<string> read_file(string filename) {
     return contents;
 }
 
+void set_variables(vector<string> contents, vector<string> productions) {
+    number_of_tokens = contents.size();
+    number_of_productions = productions.size();
 
+    for(int i = 0; i < contents.size(); i++) {
+        vector<string> temp = split(contents[i].c_str(), ' ');
+        if(std::find(terminals.begin(), terminals.end(), temp[0]) == terminals.end())
+            terminals.push_back(temp[0]);
+    }
+
+    for(int i = 0; i < productions.size(); i++) {
+        vector<string> temp = split(productions[i].c_str(), ' ');
+        if(std::find(nonterminals.begin(), nonterminals.end(), temp[0]) == nonterminals.end())
+            nonterminals.push_back(temp[0]);
+    }
+}
 
 int main() {
     vector<string> contents = read_file("grammar.txt");
     vector<string> productions;
+
     while(!contents.back().empty()) {
         productions.push_back(contents.back());
         contents.pop_back();
     }
+
     reverse(productions.begin(), productions.end());
+
+    set_variables(contents, productions);
 
     return 0;
 }
