f472bc41e3553950ce6e0a79c26e0f2c4625480d
Dylan Wulf <wulfd1@tcnj.edu>
2016-10-07 03:44:58 -0400

    Added function to calculate firsts, it's not done yet.

--- parser_generator.cpp
+++ parser_generator.cpp
@@ -99,6 +99,67 @@ vector< vector<int> > fill_productions(map<string, int> terminals, map<string, i
     return prod_out;
 }
 
+vector<bool> fill_eps(vector< vector<int> > prods, vector<int> prod_to_nons, int num_nonterms) {
+    vector<bool> out(num_nonterms + 1, false);
+    for (int i = 0; i < prods.size(); i++) {
+        if (prods[i].empty()) {
+            out[prod_to_nons[i]] = true;
+        }
+    }
+    return out;
+}
+
+bool vector_contains(vector<int> v, int n) {
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == n)
+            return true;
+    }
+    return false;
+}
+
+//add v2 stuff into v1
+vector<int> vector_union(vector<int> v1, vector<int> v2) {
+    vector<int> out(v1);
+    for (int i = 0; i < v2.size(); i++) {
+        if (!vector_contains(out, v2[i])) {
+            out.push_back(v2[i]);
+        }
+    }
+    return out;
+}
+
+vector< vector<int> > fill_first(vector< vector<int> > prods, vector<bool> eps, vector<int> prod_to_nons, int num_non_terms) {
+    vector< vector<int> > firsts;
+    for (int i = 0; i < num_non_terms; i++) //make a vector for each nonterminal
+        firsts.push_back(vector<int>());
+
+    for(int lhs = 0; lhs < prods.size(); lhs++) {
+        for (int rhs = 0; rhs < prods[lhs].size(); rhs++) {
+            int term = prods[lhs][rhs];
+            int first_loc = prod_to_nons[lhs];
+            if (term < 0) {
+                firsts[first_loc] = vector_union(firsts[first_loc], vector<int>(1, term));
+            }
+            else
+                firsts[first_loc] = vector_union(firsts[first_loc], firsts[term]);
+            if (term > 0 && eps[term-1] == false)
+                rhs = prods[lhs].size();
+        }
+    }
+    return firsts;
+}
+
+vector<int> prod_to_nonterms(vector<string> productions, map<string, int> nonterms) {
+    vector<int> out;
+    out.push_back(0);
+    out[0] = 0; //productions start at #1, so we'll just ignore index 0
+    for (int i = 1; i < productions.size() + 1; i++) {
+        string nonterm_str = split(split(productions[i-1], "->")[0], " ")[0];
+        out.push_back(nonterms[nonterm_str]);
+    }
+    return out;
+}
+
 /*
     assigns values to the variables declared at the beginning
  */
@@ -141,12 +202,16 @@ int main() {
     map<string, int> terminals_map = fill_terminals(terminals);
     map<string, int> nonterminals_map = fill_nonterminals(productions);
 
-    vector< vector<int> > prods = fill_productions(terminals_map, nonterminals_map, productions);
+    vector<int> prod_to_nons = prod_to_nonterms(productions, nonterminals_map);
 
-    for (vector< vector<int> >::iterator it = prods.begin(); it != prods.end(); it++) {
-        reverse((*it).begin(), (*it).end());
-        for (vector<int>::iterator jt = (*it).begin(); jt != (*it).end(); jt++)
-            cout << *jt << ", ";
+    vector< vector<int> > prods = fill_productions(terminals_map, nonterminals_map, productions);
+    vector<bool> eps = fill_eps(prods, prod_to_nons, nonterminals_map.size());
+
+    vector< vector<int> > firsts = fill_first(prods, eps, prod_to_nons, nonterminals_map.size());
+    for (int i = 0; i < firsts.size(); i++) {
+        for (int j = 0; j < firsts[i].size(); j++) {
+            cout << firsts[i][j] << ", ";
+        }
         cout << endl;
     }
 
